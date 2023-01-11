5a348bc2d248d7af727e00a5d16bcc8994e66744
Paulo Vitor <paulo.vitor@jovensgenios.com>
2023-01-11 00:17:32 -0300

    feat: first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,27 @@
+import { CreateNotificationBody } from './create-notification-body';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
